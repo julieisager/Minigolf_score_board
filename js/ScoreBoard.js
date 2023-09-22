f5886986f2bc43921dc0b8165e5976817d8cfe23
Julie <julie@isager.dk>
2023-09-22 07:35:37 +0200

    commit

---
+++ js/ScoreBoard.js
@@ -0,0 +1,48 @@
+
+
+export default class ScoreBoard{
+
+
+    constructor() {
+
+        const minusButtons = document.querySelectorAll('.minus');
+        const plusButtons = document.querySelectorAll('.plus');
+        const counters = document.querySelectorAll('.counter');
+
+
+        for (let i = 0; i < minusButtons.length; i++) {
+            minusButtons[i].addEventListener('click', () => {
+                counters[i].value = parseInt(counters[i].value) - 1;
+                updateTotal();
+            });
+
+            plusButtons[i].addEventListener('click', () => {
+                counters[i].value = parseInt(counters[i].value) + 1;
+                updateTotal();
+            });
+        }
+
+
+        function updateTotal() {
+            let sum = 0;
+            for (let i = 0; i < counters.length; i++) {
+                sum += parseInt(counters[i].value);
+            }
+            total.value = sum;
+        }
+
+
+        const resetButton = document.querySelector('.reset');
+        resetButton.addEventListener('click', () => {
+            const counters = document.querySelectorAll('.counter');
+            for (let i = 0; i < counters.length; i++) {
+                counters[i].value = '0';
+            }
+            updateTotal();
+        });
+
+
+    }
+
+
+}
